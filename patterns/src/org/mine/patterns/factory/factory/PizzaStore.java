d9f1c7e43219bdcb0a3453e37d7cb688060be663
ryan <zyzcharry@163.com>
2021-03-24 21:48:21 +0800

    工厂模式

--- patterns/src/org/mine/patterns/factory/factory/PizzaStore.java
+++ patterns/src/org/mine/patterns/factory/factory/PizzaStore.java
@@ -4,6 +4,10 @@ import org.mine.patterns.factory.Pizza;
 
 /**
  *工厂方法模式
+ * abstract Product factoryMethod(String type)
+ * 所有的工厂模式都用来封装对象的创建。工厂方法模式通过让子类决定该创建的对象是什么，来达到将对象
+ * 创建的过程封装的目的。
+ * 工厂方法模式定义了一个创建对象的接口，但由子类决定要实例化的类是哪一个。工厂方法让类把实例化推迟到子类。
  */
 public abstract class PizzaStore {
     public Pizza orderPizza(String type) {
